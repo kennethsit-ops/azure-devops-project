de27e9907a2f855fef1c1edce2fcb97644b34dfd
kennethsit-ops <sitkambackup@gmail.com>
2026-07-01 11:03:22 -0700

    Add Azure Storage Account with Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,17 @@
 variable "rg_name" {
   description = "Azure Resource Group Name."
   type        = string
-  default     = "rg-devops-1"
+  default     = "rg-devops-dev"
 }
 
 variable "location" {
   description = "Azure Region."
   type        = string
-  default     = "Canada East"
+  default     = "Canada Central"
+}
+
+variable "storage_account_name" {
+  description = "Azure Storage Account Name."
+  type        = string
+  default     = "stdevops001"
 }
