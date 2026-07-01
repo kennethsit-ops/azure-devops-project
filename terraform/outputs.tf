c7bdde9d7adc10f738905f3749b05f406d54bf84
kennethsit-ops <sitkambackup@gmail.com>
2026-07-01 11:03:17 -0700

    Add Azure Storage Account with Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "resource_group_id" {
   description = "The ID of the resource group"
   value = azurerm_resource_group.rg.id
 }
+
+output "storage_account_name" {
+  description = "The name of the storage account"
+  value = azurerm_storage_account.storage.name
+}
