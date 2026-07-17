38cb56e394b697addf0d274576a89321b97b3b80
kennethsit-ops <sitkambackup@gmail.com>
2026-07-16 17:42:39 -0700

    update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+HOSTNAME=$(hostname)
+CURRENT_USER=$(whoami)
+CURRENT_TIME=$(date)
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
+
 set -u 
 echo "====================================="
 echo "VM Health Check"
 echo "====================================="
 
-echo "Hostname: $(hostname)"
-echo "Current user: $(whoami)"
-echo "Date: $(date)"
-echo "Private IP: $(hostname -I | awk '{print $1}')"
+echo "Hostname      : $HOSTNAME"
+echo "Current user  : $CURRENT_USER"
+echo "Date          : $CURRENT_TIME"
+echo "Private IP    : $IP_ADDRESS"
