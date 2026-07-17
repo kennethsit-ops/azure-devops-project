33c892405d5a45ec2fcb60f43489b3df3fb48f76
kennethsit-ops <sitkambackup@gmail.com>
2026-07-16 18:14:24 -0700

    update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
+set -u 
 HOSTNAME=$(hostname)
 CURRENT_USER=$(whoami)
 CURRENT_TIME=$(date)
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
+print_header() {
 
-set -u 
-echo "====================================="
-echo "VM Health Check"
-echo "====================================="
+    echo "====================================="
+    echo "VM Health Check"
+    echo "====================================="
+    echo "Hostname      : $HOSTNAME"
+    echo "Current user  : $CURRENT_USER"
+    echo "Date          : $CURRENT_TIME"
+    echo "Private IP    : $IP_ADDRESS"
+}
 
-echo "Hostname      : $HOSTNAME"
-echo "Current user  : $CURRENT_USER"
-echo "Date          : $CURRENT_TIME"
-echo "Private IP    : $IP_ADDRESS"
+
+check_command() {
+    local command=$1
+    if command -v "$command" &> /dev/null; then
+        echo "$command is installed"
+    else
+        echo "$command is not installed"
+    fi
+}
 
 
-if [command -v git &> /dev/null]; then
-    echo "Git is installed"
-else
-    echo "Git is not installed"
-fi
 
-if [command -v docker &> /dev/null]; then
-    echo "Docker is installed"
-else
-    echo "Docker is not installed"
-fi
+main() {
+    print_header
+    echo "-------------------------------------"
+    check_command git
+    check_command docker
+    check_command terraform
+    check_command az    
+}
+
+main
