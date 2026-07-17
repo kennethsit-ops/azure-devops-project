317e904a9afbbbea82ac907592e2a904766995a1
kennethsit-ops <sitkambackup@gmail.com>
2026-07-16 18:03:40 -0700

    update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -15,3 +15,16 @@ echo "Hostname      : $HOSTNAME"
 echo "Current user  : $CURRENT_USER"
 echo "Date          : $CURRENT_TIME"
 echo "Private IP    : $IP_ADDRESS"
+
+
+if [command -v git &> /dev/null]; then
+    echo "Git is installed"
+else
+    echo "Git is not installed"
+fi
+
+if [command -v docker &> /dev/null]; then
+    echo "Docker is installed"
+else
+    echo "Docker is not installed"
+fi
